c9b84625542f489f66c3b6775a375c8f0eaee933
Bradley <brlostak@asparis.fr>
2016-02-24 15:22:15 +0100

    Nothing

--- app/src/main/java/com/example/bradley/geoquiz/QuizActivity.java
+++ app/src/main/java/com/example/bradley/geoquiz/QuizActivity.java
@@ -1,29 +1,37 @@
 package com.example.bradley.geoquiz;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
     private Button mTrueButton;
     private Button mFalseButton;
-    private Button mNextButton;
+    private ImageButton mNextButton;
+    private ImageButton mBackButton;
+    private Button mCheatButton;
     private TextView mQuestionTextView;
+    private static final String TAG = "QuizActivity";
+    private static final String KEY_INDEX = "index";
 
     private Question[] mQuestionBank = new Question[]{
-            new Question(R.string.question_africa, false),
-            new Question(R.string.question_america,true),
-            new Question(R.string.question_asia, true),
-            new Question(R.string.question_ocean, true),
-            new Question(R.string.question_mideast, false)
+            new Question(R.string.question_bradley,true),
+            new Question(R.string.question_mira,false),
+            new Question(R.string.question_sean, true),
+            new Question(R.string.question_matt, true)
     };
 
     private int mCurrentIndex = 0;
@@ -47,6 +55,7 @@ public class QuizActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate(Bundle) called");
         setContentView(R.layout.activity_quiz);
 
         mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
@@ -56,12 +65,13 @@ public class QuizActivity extends AppCompatActivity {
         mTrueButton = (Button) findViewById(R.id.true_button);
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
-        public void onClick(View v){
+            public void onClick(View v) {
                 checkAnswer(true);
 
             }
 
         });
+
         mFalseButton = (Button) findViewById(R.id.false_button);
         mFalseButton.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -71,7 +81,7 @@ public class QuizActivity extends AppCompatActivity {
 
             }
         });
-        mNextButton = (Button) findViewById(R.id.next_button);
+        mNextButton = (ImageButton) findViewById(R.id.next_button);
         mNextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -79,7 +89,74 @@ public class QuizActivity extends AppCompatActivity {
                updateQuestion();
             }
         });
+        mBackButton = (ImageButton) findViewById(R.id.back_button);
+        mBackButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                if(mCurrentIndex != 0) {
+                    mCurrentIndex = (mCurrentIndex - 1) % mQuestionBank.length;
+                    updateQuestion();
+                }
+            }
+
+        });
+       // if (savedInstanceState != null) {           //Ask Mr.Schildge
+         //   mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
+       // }
+        mCheatButton = (Button)findViewById(R.id.cheat_button);
+        mCheatButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent i = new Intent(QuizActivity.this, CheatActivity.class);
+                try {
+                    startActivity(i);
+                }
+                catch (ActivityNotFoundException x){
+                    Log.w(TAG, "ActivityNotFoundException on call of: startActivity(i)");
+                }
+            }
+        });
+
+        try {
+            mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
+        }
+        catch (NullPointerException x) {
+            Log.w(TAG, "NullPointerException on call of: mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0)");
+        }
+
         updateQuestion();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState){
+        super.onSaveInstanceState(savedInstanceState);
+        Log.i(TAG, "onSaveInstanceState");
+        savedInstanceState.putInt(KEY_INDEX, mCurrentIndex);
+    }
+    @Override
+    public void onStart(){
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
+    @Override
+    public void onPause(){
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume() called");
+    }
+    @Override
+    public void onStop(){
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+    }
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
     }
 
     @Override
